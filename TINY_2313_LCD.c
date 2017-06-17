9fbf302da45dcc10dc0d899e5c6f3c2a1b5c574e
ashinjohn <ashinbasiljohn@gmail.com>
2017-06-17 23:49:58 +0530

    Programming complete

--- TINY_2313_LCD.c
+++ TINY_2313_LCD.c
@@ -1,3 +1,12 @@
+/* Automatic Water Level Controller
+ * Microcontroller : AT Tiny 2313
+ * TINY_2313_LCD.c
+ *
+ * Created: 17/06/17 09:12:36
+ *  Author: Ashin Basil John
+ */ 
+
+
 #include<avr/io.h>
 #define F_CPU 16000000UL // internal clock
 #include <util/delay.h>
@@ -5,19 +14,26 @@
 
 void lcd_cmd (char cmd);
 void lcd_data (char dat);
+void printlcd(const char *CPtr);
+
+#define safety (PIND & 0x08) // input from safety probe  
+#define high (PIND & 0x10) // input from high probe
+#define low (PIND & 0x20) // input from low probe
+
+#define pump_on (PORTD |= _BV(PORTD0))   //switch ON PUMP
+#define pump_off (PORTD &= ~_BV(PORTD0))  //switch OFF PUMP
+
+#define buzz_on (PORTD |= _BV(PORTD1))   //switch ON Buzzer
+#define buzz_off (PORTD &= ~_BV(PORTD1))  //switch OFF Buzzer
 
 
 
 int main (void)
 {
-char let[] ={ " ashin "} ;
 int i;
 
-DDRB = 0xFF;  
-
-//  intialising lcd 
-/* require 2 line display
-   to improve sucess rates proving 2 one line also */
+DDRB = 0xFF; // for LCD module
+DDRD = 0xC7; // for buzzer and probes
 
 lcd_cmd(0x33);  // one line
 lcd_cmd(0x32);  
@@ -28,58 +44,92 @@ lcd_cmd(0x01);  // clear display
 lcd_cmd(0x80);  // first row first location
 
 
+printlcd("PUMP CONTROLLER");
+lcd_cmd(0xC0); 
+printlcd("SYSTEM READY");
+buzz_on;
+_delay_ms(300);
+buzz_off;
 
-while (1) // non recuring loop
+int error=0;
+int low_count = 0;
+int high_count = 0;
+int filling_tank = 0;
+
+
+while (1)
 {
 
-for (i=0;i<strlen(let);i++)
-{
-lcd_data(let[i]);  // data to show on lcd
+	if (safety==0){
+		pump_off;
+		error=1;
+		filling_tank=0;
+		lcd_cmd(0x80);  
+		printlcd("PROBE ERROR     ");
+	}
+	else if (high==0){
+		high_count++;
+		_delay_ms(100);
+		
+		if (high_count>10){
+			high_count = 0;
+			pump_off;
+	
+			lcd_cmd(0xC0);
+			printlcd("TANK FULL     ");
+			_delay_ms(300);
+			lcd_cmd(0xC0);
+			printlcd("PUMP OFF     ");
+			filling_tank=0;
+			buzz_off;
+		}
+	}		
+	else if (low==0){
+		low_count++;
+		_delay_ms(100);
+		
+		if (low_count>10){
+			pump_on;
+			low_count=0;
+			filling_tank=1;
+			lcd_cmd(0xC0);
+			printlcd("TANK EMPTY     ");
+			_delay_ms(300);
+			lcd_cmd(0xC0);
+			printlcd("PUMP ON      ");
+			_delay_ms(300);
+			lcd_cmd(0xC0);	
+		}
+	}
+
+	else if(filling_tank==1){
+		lcd_cmd(0xC0);
+		printlcd("FILLING TANK     ");
+		buzz_on;
+		_delay_ms(100);
+		buzz_off;
+	}
+
 }
-
-
-}
-
 }
 
 void lcd_cmd (char cmd) // deals with command register
 {
-/*.............CONNECTION.............. 
 
-   	PORT C - RS RW E
-			  7  6 5
-			  
-			  RS 0 - command register
-			  RS 1 - data 	 register
-
-	PORT D - D0 D1 .... D7    8 bit data
-			
-    0x 'x' for hexadecimal
-*/
 
 PORTB &=(~0x80); // setting RS 0 for command register
 
-	/*  setting the first bit low , need for providing 
-	
-	80 - 1000 0000 
-	~    0111 1111
-	&    0   	- 0XXX XXXX
-	     1		- 0XXX XXXX
-	
-	find hex with high on the pin needed.	
-	do &=(~  to set that pin as low
-	do |=        				high
-	*/
+
 
 PORTB &=(~0x40); // X0XX XXXX  setting ~write
 				 
-PORTB = (PORTD & 0xF0)|((cmd>>4) & 0x0F); // send higher nibble
+PORTB = (PORTB & 0xF0)|((cmd>>4) & 0x0F); // send higher nibble
 PORTB |= 0x20; 		// enable high
 PORTB &= (~0x20);	// enable low
 _delay_ms(10);		// delay for slow device
 
 
-PORTB = (PORTD & 0xF0)|(cmd & 0x0F); 
+PORTB = (PORTB & 0xF0)|(cmd & 0x0F); 
 
 PORTB |= 0x20; 		// enable high
 PORTB &= (~0x20);	// enable low
@@ -96,13 +146,13 @@ PORTB |= 0x80;    // high RS for data reg
 PORTB &= (~0x40); // low as writing onto lcd screen 
 
 
-PORTB = (PORTD & 0xF0)|((dat>>4) & 0x0F); // send higher nibble
+PORTB = (PORTB & 0xF0)|((dat>>4) & 0x0F); // send higher nibble
 PORTB |= 0x20; 		// enable high
 PORTB &= (~0x20);	// enable low
 _delay_ms(10);		// delay for slow device
 
 
-PORTB = (PORTD & 0xF0)|(dat & 0x0F); 
+PORTB = (PORTB & 0xF0)|(dat & 0x0F); 
 
 PORTB |= 0x20; 		// enable high
 PORTB &= (~0x20);	// enable low
@@ -110,6 +160,13 @@ _delay_ms(10);		// delay for slow device
 
 }
 
+void printlcd(const char *CPtr)
+{
+	while(*CPtr != '\0'){
+		lcd_data(*CPtr);
+		++CPtr;
+	}
+}
 
 
 
